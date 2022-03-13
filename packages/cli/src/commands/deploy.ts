325f471c21665fe46ff1ec39026d73156acbd43d
Fotis Alexandrou <falexandrou@gmail.com>
2022-03-13 13:48:36 +0200

    scaffold deploy command

---
+++ packages/cli/src/commands/deploy.ts
@@ -0,0 +1,28 @@
+import { Command } from '@oclif/core';
+
+import { DeployOperation } from '@stackmate/engine';
+
+class DeployCommand extends Command {
+  /**
+   * @var {String} description the command's description
+   */
+  static description: string = 'Deploy your infrastructure based to the cloud';
+
+  /**
+   * @var {Array} args the command's arguments
+   */
+  static args = [
+    { name: 'stage', description: 'The stage to deploy', required: true },
+  ];
+
+  async run(): Promise<void> {
+    const { args: { stage } } = await this.parse(DeployCommand);
+
+    const projectFile = '';
+    const operation = DeployOperation.factory(projectFile, stage);
+
+    await operation.run();
+  }
+}
+
+export default DeployCommand;
